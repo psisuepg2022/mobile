864096a34cde4964bdd7e2a8557188b5d752cdd6
Renato Ruppel <heruppel2@gmail.com>
2022-12-07 20:15:57 -0300

    feat(theme): added color palette

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF419D78);
+  static const secondary = Color(0xFF009686);
+  static const darker = Color(0xFF017D6F);
+  static const scheduled = Color(0xFFD5E2D1);
+  static const confirmed = Color(0xFF92b78a);
+  static const concluded = Color(0xe661b895);
+  static const lock = Color(0xFF9BB0A5);
+  static const lockDarker = Color(0xFF688877);
+  static const backgrey = Color(0xFFEEEEEE);
+  static const danger = Color(0xFFAD2626);
+  static const dangerHover = Color(0xFF8C1313);
+  static const warning = Color(0xFFD86F51);
+  static const text = Color(0xFF303030);
+  static const absence = Color(0xFFC2C2C2);
+  static const cancelled = Color(0xFFB6A2A1);
+  static const past = Color(0xFFe3e3e3);
+  static const whatsapp = Color(0xFF25D366);
+}
